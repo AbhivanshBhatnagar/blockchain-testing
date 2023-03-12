c205f71d08dbccd0d92bae7475697cd95ec2bedc
shashank <daimashashank10@gmail.com>
2023-03-13 02:31:00 +0530

    finalise the home screen.

--- lib/presentation/main/home/home_screen.dart
+++ lib/presentation/main/home/home_screen.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_project/presentation/main/home/widgets/home_tabs.dart';
 import 'package:test_project/presentation/main/home/widgets/symbol_button.dart';
-import 'dart:math' as math;
 import 'widgets/account_card.dart';
+import 'widgets/sliver_app_bar_delegate.dart';
+import 'widgets/token_tile.dart';
 
 List<String> tabsTexts = ["Tokens", "NFTs"];
 
@@ -36,17 +37,17 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
           headerSliverBuilder: (context, innerBoxIsScrolled) {
             return [
               SliverPersistentHeader(
-                delegate: _SliverAppBarDelegate(
+                delegate: SliverAppBarDelegate(
                   minHeight: 185,
                   maxHeight: 90,
                   child: const Padding(
-                    padding: const EdgeInsets.all(12.0),
+                    padding: EdgeInsets.all(12.0),
                     child: AccountCard(),
                   ),
                 ),
               ),
               SliverPersistentHeader(
-                delegate: _SliverAppBarDelegate(
+                delegate: SliverAppBarDelegate(
                   minHeight: 100,
                   maxHeight: 70,
                   child: Padding(
@@ -61,7 +62,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
                         children: [
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
+                            children: const [
                               SymbolButton(
                                   title: "Add Funds",
                                   icon: Icon(Icons.credit_card)),
@@ -74,13 +75,13 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
                           ),
                           Row(
                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
+                            children: const [
                               SymbolButton(
                                   title: "Add Funds",
                                   icon: Icon(Icons.credit_card)),
                               SymbolButton(
                                   title: "Send", icon: Icon(Icons.credit_card)),
-                              const SizedBox(
+                              SizedBox(
                                 width: 50,
                               )
                             ],
@@ -92,7 +93,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
                 ),
               ),
               SliverPersistentHeader(
-                delegate: _SliverAppBarDelegate(
+                delegate: SliverAppBarDelegate(
                   minHeight: 37,
                   maxHeight: 37,
                   child: Column(
@@ -115,7 +116,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
               ),
               SliverPersistentHeader(
                 pinned: true,
-                delegate: _SliverAppBarDelegate(
+                delegate: SliverAppBarDelegate(
                     minHeight: 50,
                     maxHeight: 50,
                     child: HomeTabs(
@@ -127,32 +128,20 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
           body: TabBarView(
             controller: _tabController,
             children: [
-              ListView.builder(key: new PageStorageKey('myListView1'),itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('ListTile${index}'),
-                  isThreeLine: true,
-                  subtitle: Text('Secondary text\nTertiary text'),
-                  leading: ClipOval(
-                      child: CachedNetworkImage(
-                    imageUrl:
-                        "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/ZJZZK5B2ZNF25LYQHMUTBTOMLU.png",
-                  )),
-                  trailing: Text('Metadata'),
-                );
-              }),
-              ListView.builder(key: new PageStorageKey('myListView2'),itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text('ListTile${index}'),
-                  isThreeLine: true,
-                  subtitle: Text('Secondary text\nTertiary text'),
-                  leading: ClipOval(
-                      child: CachedNetworkImage(
-                    imageUrl:
-                        "https://cloudfront-us-east-1.images.arcpublishing.com/coindesk/ZJZZK5B2ZNF25LYQHMUTBTOMLU.png",
-                  )),
-                  trailing: Text('Metadata'),
-                );
-              }),
+              ListView.builder(
+                  key: const PageStorageKey('myListView1'),
+                  itemBuilder: (context, idx) {
+                    return TokenTile(
+                      index: idx,
+                    );
+                  }),
+              ListView.builder(
+                  key: const PageStorageKey('myListView2'),
+                  itemBuilder: (context, index) {
+                    return TokenTile(
+                      index: index,
+                    );
+                  }),
             ],
           ),
         ),
@@ -160,66 +149,3 @@ class _HomeScreenState extends ConsumerState<HomeScreen>
     );
   }
 }
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  _SliverAppBarDelegate({
-    required this.minHeight,
-    required this.maxHeight,
-    required this.child,
-  });
-  final double minHeight;
-  final double maxHeight;
-  final Widget child;
-
-  @override
-  double get minExtent => minHeight;
-
-  @override
-  double get maxExtent => math.max(maxHeight, minHeight);
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return SizedBox.expand(child: child);
-  }
-
-  @override
-  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
-    return maxHeight != oldDelegate.maxHeight ||
-        minHeight != oldDelegate.minHeight ||
-        child != oldDelegate.child;
-  }
-}
-// children: [
-//             Padding(
-//               padding: const EdgeInsets.all(12.0),
-//               child: AccountCard(),
-//             ),
-//             Container(
-//               height: 90,
-//               child: PageView(
-//                 children: [
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: [
-//                       SymbolButton(
-//                           title: "Add Funds", icon: Icon(Icons.credit_card)),
-//                       SymbolButton(title: "Send", icon: Icon(Icons.credit_card)),
-//                       SymbolButton(
-//                           title: "Receive", icon: Icon(Icons.credit_card)),
-//                     ],
-//                   ),
-//                   Row(
-//                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                     children: [
-//                       SymbolButton(
-//                           title: "Add Funds", icon: Icon(Icons.credit_card)),
-//                       SymbolButton(title: "Send", icon: Icon(Icons.credit_card)),
-//                       SymbolButton(
-//                           title: "Receive", icon: Icon(Icons.credit_card)),
-//                     ],
-//                   ),
-//                 ],
-//               ),
-//             ),
-//             Divider(),
