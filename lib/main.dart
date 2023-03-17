82344b2a701be1cb96bf5834ac3ed2b823a725db
shashank <daimashashank10@gmail.com>
2023-03-17 11:42:41 +0530

    signup page provider scope fix.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test_project/firebase_options.dart';
 import 'app.dart';
 import 'dynamic_link_handler.dart';
@@ -13,5 +14,5 @@ Future<void> main() async {
   final PendingDynamicLinkData? initialLink =
       await FirebaseDynamicLinks.instance.getInitialLink();
 
-  runApp(App());
+  runApp(const ProviderScope(child: App()));
 }
--- lib/presentation/auth/signup_screen.dart
+++ lib/presentation/auth/signup_screen.dart
@@ -17,68 +17,104 @@ class SignupScreen extends ConsumerStatefulWidget {
 class _SignupScreenState extends AuthState<SignupScreen> {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return Scaffold(
       body: SafeArea(
         child: Center(
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 25, right: 25),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        "Let's get Started!",
-                        style: GoogleFonts.urbanist(
-                            fontWeight: FontWeight.w600, fontSize: 26),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text("An amazing web3 journey awaits"),
-                    ],
-                  ),
-                  SvgPicture.asset("assets/logo.svg",
-                      semanticsLabel: 'Acme Logo'),
-                  Row(
-                    children: [
-                      const Text("Email"),
-                    ],
-                  ),
-                  TextField(),
-                  Text("Or continue with"),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Color(0xFF25252D)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(14.0),
-                          child: SvgPicture.asset("assets/google_logo.svg"),
+          child: ScrollConfiguration(
+            behavior: const ScrollBehavior().copyWith(overscroll: false),
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 25, right: 25),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "Let's get Started!",
+                          style: GoogleFonts.urbanist(
+                              fontWeight: FontWeight.w600, fontSize: 26),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "An amazing web3 journey awaits",
+                          style: GoogleFonts.inter(color:const Color(0xFF626164)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: screenHeight * 0.15),
+                    SvgPicture.asset("assets/logo.svg",
+                        semanticsLabel: 'Acme Logo'),
+                    SizedBox(height: screenHeight * 0.125),
+                    Row(
+                      children: [
+                        Text(
+                          "Email",
+                          style: GoogleFonts.inter(
+                              fontSize: 22, fontWeight: FontWeight.w500),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextField(
+                      autofocus: false,
+                      style:
+                          TextStyle(fontSize: 17.0, color: Color(0xFFbdc6cf)),
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFF25252D),
+                        hintText: 'Email',
+                        isDense: true,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.circular(40),
                         ),
                       ),
-                      Container(
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Color(0xFF25252D)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(15.0),
-                          child: SvgPicture.asset("assets/twitter_logo.svg"),
+                    ),
+                    SizedBox(height: screenHeight * 0.15 * 0.25),
+                    const Text("Or continue with"),
+                    SizedBox(height: screenHeight * 0.15 * 0.25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFF25252D)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(14.0),
+                            child: SvgPicture.asset("assets/google_logo.svg"),
+                          ),
                         ),
-                      ),
-                      Container(
-                        decoration: const BoxDecoration(
-                            shape: BoxShape.circle, color: Color(0xFF25252D)),
-                        child: Padding(
-                          padding: const EdgeInsets.all(18.0),
-                          child: SvgPicture.asset("assets/fb_logo.svg"),
+                        Container(
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFF25252D)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: SvgPicture.asset("assets/twitter_logo.svg"),
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        Container(
+                          decoration: const BoxDecoration(
+                              shape: BoxShape.circle, color: Color(0xFF25252D)),
+                          child: Padding(
+                            padding: const EdgeInsets.all(18.0),
+                            child: SvgPicture.asset("assets/fb_logo.svg"),
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
