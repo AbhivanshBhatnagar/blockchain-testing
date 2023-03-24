f9d91491f44a84744f547c70b6997ff4b5e8e8de
Antonio Pedro <tonio.pedro99@gmail.com>
2023-03-24 09:13:11 +0530

    nft screen

--- lib/presentation/main/nft/nft_screen.dart
+++ lib/presentation/main/nft/nft_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:test_project/presentation/main/nft/widgets/collection_tile.dart';
-import 'package:test_project/presentation/nft_details/nft_details_screen.dart';
+import 'package:test_project/presentation/main/nft_details/nft_details_screen.dart';
 
 class NFTCollectionModel {
   final String? image;
