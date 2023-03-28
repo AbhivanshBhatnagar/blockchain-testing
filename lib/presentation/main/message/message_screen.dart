21fae843d2dce363915aa16f4e4c47d1eb7c53e7
shashank <daimashashank10@gmail.com>
2023-03-28 22:04:20 +0530

    comming soon screens.

--- lib/presentation/main/message/message_screen.dart
+++ lib/presentation/main/message/message_screen.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MessageScreen extends StatelessWidget {
   const MessageScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("Message "));
+    return Scaffold(body: Center(child: Text("Messaging Comming soon.", style: GoogleFonts.inter(fontSize: 22),)));
+
+    // return Scaffold(body: Text("Message "));
   }
 }
